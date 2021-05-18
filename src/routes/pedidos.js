e8d4a11b2fd3201b18f96dd0d05add821c3408a7
darodweb <darodweb@gmail.com>
2021-05-17 23:00:24 -0300

    Update to pedidos

--- src/routes/pedidos.js
+++ src/routes/pedidos.js
@@ -5,7 +5,11 @@ const actions = require('../actions');
 const authentication = require('../authentication');
 
 router.get('/pedidos', authentication.verifyUser, async (req, res) => {
-    const pedidos = await actions.get('SELECT * FROM pedidos');
+    const pedidos = await actions.get(`SELECT est.Name as State, o.time, o.number, o.description, o.total,
+    u.Name as User, u.Addres, o.idpaymentType 
+    FROM Pedido as ped 
+    Inner JOIN estado as est ON (s.id = ped.state) 
+    INNER JOIN usuario as u ON (u.idUser = ped.idUser)`);
     res.send(pedidos);
 });
 
@@ -15,7 +19,7 @@ router.get('/pedido/:id', authentication.verifyUser, async (req, res) => {
     res.send(pedido);
 });
 
-router.post('/pedido/', authentication.verifyAdmin, async (req, res) => {
+router.post('/pedido/', authentication.verifyUser, async (req, res) => {
     const pedido = await actions.create(
         `INSERT INTO pedidos (foto, descripcion, precio) VALUES (:foto, :descripcion, :precio)`,
         req.body);
