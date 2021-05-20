f28fcbd04465f01ac3ecf883fbcf37f64cedcedd
darodweb <darodweb@gmail.com>
2021-05-19 22:56:09 -0300

    Update to pedidos endpoint

--- src/routes/pedidos.js
+++ src/routes/pedidos.js
@@ -5,11 +5,12 @@ const actions = require('../actions');
 const authentication = require('../authentication');
 
 router.get('/pedidos', authentication.verifyUser, async (req, res) => {
-    const pedidos = await actions.get(`SELECT est.Name as State, o.time, o.number, o.description, o.total,
-    u.Name as User, u.Addres, o.idpaymentType 
-    FROM Pedido as ped 
-    Inner JOIN estado as est ON (s.id = ped.state) 
-    INNER JOIN usuario as u ON (u.idUser = ped.idUser)`);
+    const pedidos = await actions.get(`SELECT e.id as Estado, p.hora as Hora, p.id as Numero, p.descripcion as Descripcion,
+    f.id as Forma_de_Pago, p.total as Total, u.nombre as Usuario,
+    u.direccion as Direccion FROM pedidos AS p
+    INNER JOIN usuarios AS u ON (p.usuario_id = u.id)
+    INNER JOIN forma_de_pago as f ON (p.forma_de_pago = f.id)
+    INNER JOIN estados as e ON (e.id = p.estado )`);
     res.send(pedidos);
 });
 
